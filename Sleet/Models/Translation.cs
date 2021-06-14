0ead498a7743a16476dc17f303b968fb5d0cd5c3
baka-gourd <fanng233@gmail.com>
2021-06-14 22:50:45 +0800

    添加翻译元素基础功能
    update

--- Sleet/Models/Translation.cs
+++ Sleet/Models/Translation.cs
@@ -1,11 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Sleet.Models
-{
-    public class Translation
-    {
+namespace Sleet.Models {
+    public class Translation {
+        public string Key { get => GetKey(); set => throw new ArgumentException("Key cannot change!"); }
+        public bool OriginalLanguage { get => IsOriginalLanguage(); set => OriginalLanguage = value; }
+        public LanguageCode Language { get => GetLanguageCode(); set => Language = value; }
+        public string Content { get => GetContent(); set => Content = value; }
+
+        public bool IsOriginalLanguage() => OriginalLanguage;
+
+        public void SetOriginalLanguage(bool boolean) => OriginalLanguage = boolean;
+
+        public LanguageCode GetLanguageCode() => Language;
+
+        public string GetContent() => Content;
+
+        public string GetKey() => Key;
+
+        public KeyValuePair<string, string> GetKeyValuePair() => new(Key, Content);
+
+    }
+
+    public enum LanguageCode {
+        ZhCn,
+        ZhTw,
+        EnUs
     }
 }
