3591ab6c30f5216e479dccd86b668b8e8235765e
baka-gourd <fanng233@gmail.com>
2021-06-18 14:30:39 +0800

    update

---
+++ Sleet/Generator/ITranslationGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Sleet.Models;
+
+namespace Sleet.Generator {
+    public interface ITranslationGenerator {
+        IEnumerable<Translation> Generate();
+        public bool CanGenerate { get; set; }
+    }
+}
--- Sleet/Models/Component.cs
+++ Sleet/Models/Component.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 namespace Sleet.Models
 {
     public class Component {
-        public Guid Id { get; set; }
+        public Component(Guid id, string name, List<Translation[]> translationsList, bool isLock = false, object description = null) {
+            Id = id;
+            Description = description;
+            Name = name;
+            IsLock = isLock;
+            TranslationsList = translationsList;
+        }
+
+        public Guid Id { get; private set; }
         public Object Description { get; set; }
         public string Name { get; set; }
+        public bool IsLock { get; set; }
         public List<Translation[]> TranslationsList { get; set; }
     }
     //public class Component
