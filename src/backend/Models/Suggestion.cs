5d0b7584c353b7ce6b95be504529dffebaab8422
summpot <summpot@outlook.com>
2021-06-22 07:20:02 +0800

    Add user

--- src/backend/Models/Suggestion.cs
+++ src/backend/Models/Suggestion.cs
@@ -5,9 +5,9 @@ namespace Sleet.Models
     public class Suggestion
     {
         public Guid Id { get; set; }
-        public string User { get; set; } //这个要上新类
+        public User User { get; set; }
         public string Content { get; set; }
         public bool Approved { get; set; }
-        public string ApproveUser { get; set; } //这个也要上新类
+        public User Approver { get; set; }
     }
 }
---
+++ src/backend/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Sleet.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Claim> Claims { get; set; }
+    }
+}
