4a785d33cf1962c70b447234e3a0b5a8dbab1d6a
baka-gourd <fanng233@gmail.com>
2021-06-25 21:17:08 +0800

    update

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { TranslationRulesComponent } from './home/translation-rules/translation-rules.component'
+import { WelcomeComponent } from './home/welcome/welcome.component'
 
 const routes: Routes = [
   { path: 'projects', component: ProjectsComponent },
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home', component: HomeComponent, children: [
+      { path: '', redirectTo: '/home/welcome', pathMatch: 'full' },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'translation-rules', component: TranslationRulesComponent }
+    ]
+  },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: '**', component: HomeComponent }
 ];
 
---
+++ src/frontend/src/app/home/translation-rules/translation-rules.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-translation-rules',
+  templateUrl: './translation-rules.component.html',
+  styleUrls: ['./translation-rules.component.css']
+})
+export class TranslationRulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
