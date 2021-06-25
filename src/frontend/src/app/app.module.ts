772d358bf00734039a9bd8332f38627a1215ddfc
baka-gourd <fanng233@gmail.com>
2021-06-25 22:17:49 +0800

    update

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectComponent } from './project/project.component';
+import { ProjectComponent } from './projects/project/project.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
@@ -12,8 +12,8 @@ import { WelcomeComponent } from './home/welcome/welcome.component';
 
 @NgModule({
   declarations: [AppComponent, ProjectComponent, ProjectsComponent, HomeComponent, NavComponent, TranslationRulesComponent, WelcomeComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule,ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent,NavComponent],
+  bootstrap: [AppComponent, NavComponent],
 })
-export class AppModule {}
+export class AppModule { }
